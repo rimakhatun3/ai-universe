a55256414f92274937f9adfd0dc3799989c20b92
rimakhatun3 <rimamnt46@gmail.com>
2023-03-03 11:06:42 +0600

    added modal

--- js/feature.js
+++ js/feature.js
@@ -3,6 +3,7 @@ const loadFeature=(datalimit)=>{
     fetch(url)
     .then(res=>res.json())
     .then(data=>displayFeature(data.data,datalimit))
+  
 }
 
 const displayFeature=(data,datalimit)=>{
@@ -10,14 +11,15 @@ const displayFeature=(data,datalimit)=>{
     const cardContainer= document.getElementById('card-container')
     cardContainer.innerHTML ='';
     const seeAll = document.getElementById("see-all")
-    // if(datalimit && data.length>6){
-    // //  data = data.slice(0,6) 
-    //   seeAll.classList.remove('d-none')
-    // }
-    // else{
-    //   seeAll.classList.add('d-none')
-    // }
-    data.tools.slice(0,6).forEach(singleFeture => {
+    if(data.length>6){
+     data = data.tools.slice(0,6) 
+      seeAll.classList.remove('d-none')
+    }
+    else{
+      seeAll.classList.add('d-none')
+    }
+    
+    data.tools.forEach(singleFeture => {
         console.log(singleFeture);
         const cardDiv = document.createElement('div')
         cardDiv.classList.add('col')
@@ -34,7 +36,7 @@ const displayFeature=(data,datalimit)=>{
                 <div><h5>${singleFeture.name}</h5>
                 <p><i class="fa-solid fa-calendar-days"></i> ${singleFeture.published_in}</p></div>
                 <div>
-                <i class="fa-solid fa-arrow-right"></i>
+                <i class="fa-solid fa-arrow-right" onclick="loadSingleData('${singleFeture.id}')" data-bs-toggle="modal" data-bs-target="#staticBackdrop"></i>
                 </div>
             </div>
           </div>
@@ -43,7 +45,7 @@ const displayFeature=(data,datalimit)=>{
         cardContainer.appendChild(cardDiv);
     }); 
     
-    toggleSpiner(false) 
+    // toggleSpiner(false) 
 }
 
 const processSearch = datalimit =>{
@@ -66,6 +68,63 @@ document.getElementById('btn-see-all').addEventListener('click',function(){
 processSearch()
 })
 
+const loadSingleData =(id)=>{
+  const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`
+  fetch(url)
+  .then(res=>res.json())
+  .then(data=>displayData(data.data))
+}
+
+const displayData =data=>{
+  console.log(data)
+  const modalBody = document.getElementById('modal-body')
+  const cardDiv = document.createElement('div')
+        cardDiv.classList.add('col')
+        cardDiv.innerHTML=`
+           <div class="card h-100">
+           <div class="card-body bg-danger-subtle">
+           <div class="d-flex flex-column justify-content-center align-items-center gap-5 w-50" >
+                <div class="d-flex"><p class="card-detail">${data.description}</p></div>
+                <div class="d-flex gap-5" >
+                <div class="border rounded bg-light px-2 py-4 text-success"><p>${data.pricing[0].price}</p></div>
+                <div class="border text-warning rounded bg-light px-2 py-4"><p>${data.pricing[1].price}</p></div>
+                
+                <p class="text-danger border rounded bg-light px-4 py-2">${data.pricing[2].price}</p></div>
+                
+              </div>
+              <div class="d-flex justify-content-between me-4">
+              <div>
+              <h5 class="card-title">Features</h5>
+              <li>${data.features['1'].feature_name}</li>
+              <li>${data.features['2'].feature_name}</li>
+              <li>${data.features['3'].feature_name}</li></div>
+              <div>
+              <h5 class="card-title">Integrations</h5> 
+              <li>${data.integrations[0]}</li>
+              <li>${data.integrations[1]}</li>
+              <li>${data.integrations[2]}}</li>
+
+              </div>
+
+              <div class="card w-50 min-vh-100" >
+              
+  <img src="${data.image_link[0]}" class="card-img-top position-absolute" 
+   
+  alt="...">
+ <button class="btn btn-danger position-relative ms-5 my-2" >Accuray</button>
+  <div class="card-body">
+  
+  <h5>Hi, how are you doing today?</h5>
+    <p class="card-text">${data.accuracy.description}</p>
+  </div>
+</div>
+              </div>
+           </div>
+           </div>
+         
+        `
+        modalBody.appendChild(cardDiv)
+}
 
 
 loadFeature();
