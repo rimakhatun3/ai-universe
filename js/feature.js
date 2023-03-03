192677615d7c4a1c13eb8f297819150a2b7449f4
rimakhatun3 <rimamnt46@gmail.com>
2023-03-03 17:21:52 +0600

    modal and accury button add

--- js/feature.js
+++ js/feature.js
@@ -1,30 +1,89 @@
-const loadFeature=(datalimit)=>{
+let fetchData =[]
+const loadFeature=()=>{
+  document.getElementById('loader').classList.remove("d-none")
     url=`https://openapi.programming-hero.com/api/ai/tools`
     fetch(url)
     .then(res=>res.json())
-    .then(data=>displayFeature(data.data,datalimit))
+    .then(data=>{
+      document.getElementById('loader').classList.add("d-none");
+      displayFeature(data.data)
+      fetchData = data.data;
+      console.log(fetchData);
+    })
   
 }
 
-const displayFeature=(data,datalimit)=>{
+const displayFeature=(data)=>{
     console.log(data)
     const cardContainer= document.getElementById('card-container')
     cardContainer.innerHTML ='';
     const seeAll = document.getElementById("see-all")
-    if(data.length>6){
-     data = data.tools.slice(0,6) 
-      seeAll.classList.remove('d-none')
-    }
-    else{
-      seeAll.classList.add('d-none')
-    }
-    
-    data.tools.forEach(singleFeture => {
+    // if(data.length>6){
+    //  data = data.tools.slice(0,6) 
+    //   seeAll.classList.remove('d-none')
+    // }
+    // else{
+    //   seeAll.classList.add('d-none')
+    // }
+   
+    data = data.tools.slice(0,6)
+    data.forEach(singleFeture => {
         console.log(singleFeture);
         const cardDiv = document.createElement('div')
         cardDiv.classList.add('col')
         cardDiv.innerHTML=`
             <div class="card h-100">
+              <img  src="${singleFeture.image}" class="card-img-top border-rounded  px-4 py-4" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Features</h5>
+                <p>1.${singleFeture.features[0]?singleFeture.features[0]:'not available'}</p>
+                <p>2.${singleFeture.features[1]?singleFeture.features[1]:'not available'}</p>
+                <p>3.${singleFeture.features[2]?singleFeture.features[2]:'not available'}</p>
+              </div>
+              <div class="card-footer d-flex justify-content-between align-items-center">
+                <div><h5>${singleFeture.name}</h5>
+                <p><i class="fa-solid fa-calendar-days"></i> ${singleFeture.published_in}</p></div>
+                <div>
+                <i class="fa-solid fa-arrow-right" onclick="loadSingleData('${singleFeture.id}')" data-bs-toggle="modal" data-bs-target="#staticBackdrop"></i>
+                </div>
+            </div>
+          </div>
+        `
+        
+        cardContainer.appendChild(cardDiv);
+    }); 
+    
+}
+
+// const processSearch = datalimit =>{
+//   toggleSpiner(true);
+//   loadFeature(datalimit);
+// }
+
+
+
+// document.getElementById('btn-see-all').addEventListener('click',function(){
+// processSearch()
+// })
+
+const loadAllData=()=>{
+  url=`https://openapi.programming-hero.com/api/ai/tools`
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>{
+      seeAllData(data.data)
+    })
+}
+
+const seeAllData =(data)=>{
+  console.log(data)
+  data.tools.forEach(singleFeture=>{
+    const seAll = document.getElementById('see-all');
+const div = document.createElement('div');
+div.classList.add('col');
+div.innerHTML=`
+<div>
+<div class="card h-100 mt-5">
               <img  src="${singleFeture.image}" class="card-img-top border-rounded  px-4 py-4" alt="...">
               <div class="card-body">
                 <h5 class="card-title">Features</h5>
@@ -40,33 +99,15 @@ const displayFeature=(data,datalimit)=>{
                 </div>
             </div>
           </div>
-        `
-        
-        cardContainer.appendChild(cardDiv);
-    }); 
-    
-    // toggleSpiner(false) 
+
+</div>
+`
+seAll.appendChild(div)
+  })
+  
+  
+  
 }
-
-const processSearch = datalimit =>{
-  toggleSpiner(true);
-  loadFeature(datalimit);
-}
-
-
-const toggleSpiner = isLoading =>{
-  const loader = document.getElementById('loader')
-  if(isLoading){
-loader.classList.remove('d-none')
-  }
-  else{
-    loader.classList.add('d-none')
-  }
-}
-
-document.getElementById('btn-see-all').addEventListener('click',function(){
-processSearch()
-})
 
 const loadSingleData =(id)=>{
   const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`
@@ -86,10 +127,10 @@ const displayData =data=>{
            <div class="d-flex flex-column justify-content-center align-items-center gap-5 w-50" >
                 <div class="d-flex"><p class="card-detail">${data.description}</p></div>
                 <div class="d-flex gap-5" >
-                <div class="border rounded bg-light px-2 py-4 text-success"><p>${data.pricing[0].price}</p></div>
-                <div class="border text-warning rounded bg-light px-2 py-4"><p>${data.pricing[1].price}</p></div>
+                <div class="border rounded bg-light px-2 py-4 text-success"><p>${data.pricing[0].price?data.pricing[0].price:'no price'}</p></div>
+                <div class="border text-warning rounded bg-light px-2 py-4"><p>${data.pricing[1].price?data.pricing[1].price:'no price'}</p></div>
                 
-                <p class="text-danger border rounded bg-light px-4 py-2">${data.pricing[2].price}</p></div>
+                <p class="text-danger border rounded bg-light px-4 py-2">${data.pricing[2].price?data.pricing[0].price:'no price'}</p></div>
                 
               </div>
               <div class="d-flex justify-content-between me-4">
@@ -108,14 +149,13 @@ const displayData =data=>{
 
               <div class="card w-50 min-vh-100" >
               
-  <img src="${data.image_link[0]}" class="card-img-top position-absolute" 
+  <div class=" btn btn-danger position-relative mx-5 ">${data.accuracy.score} Accuray</div>
+  <img src="${data.image_link[0]}" class="img card-img-top" 
    
   alt="...">
- <button class="btn btn-danger position-relative ms-5 my-2" >Accuray</button>
-  <div class="card-body">
-  
-  <h5>Hi, how are you doing today?</h5>
-    <p class="card-text">${data.accuracy.description}</p>
+  <div>
+  <h5 class="px-4 py-2">Hi, how are you doing today?</h5>
+    <p class="card-text px-4 py-2">${data.description}</p>
   </div>
 </div>
               </div>
