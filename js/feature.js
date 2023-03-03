12794031550e276c7ecff10547bc5fa239d658ef
rimakhatun3 <rimamnt46@gmail.com>
2023-03-03 17:54:57 +0600

    added see all details

--- js/feature.js
+++ js/feature.js
@@ -126,8 +126,8 @@ const displayData =data=>{
            <div class="card-body bg-danger-subtle">
            <div class="d-flex flex-column justify-content-center align-items-center gap-5 w-50" >
                 <div class="d-flex"><p class="card-detail">${data.description}</p></div>
-                <div class="d-flex gap-5" >
-                <div class="border rounded bg-light px-2 py-4 text-success"><p>${data.pricing[0].price?data.pricing[0].price:'no price'}</p></div>
+                <div class="d-flex gap-5 mb-5" >
+                <div class="border rounded bg-light px-2 py-4  text-success"><p>${data.pricing[0].price?data.pricing[0].price:'no price'}</p></div>
                 <div class="border text-warning rounded bg-light px-2 py-4"><p>${data.pricing[1].price?data.pricing[1].price:'no price'}</p></div>
                 
                 <p class="text-danger border rounded bg-light px-4 py-2">${data.pricing[2].price?data.pricing[0].price:'no price'}</p></div>
